49b4b4e2a6ea2329ee7b086d8e2c9430cf429f4f
kopysovdmitry <kopysov.dmitry82@gmail.com>
2022-11-17 16:00:55 +0300

    домашнее задание по уроку String  выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        String fone = "";
+        String FirstName = "Ivan";
+        String MiddleName = "Ivanovich";
+        String LastName = "Ivanov";
+        String FullName = LastName+ " "+ FirstName+" "+FirstName;
+        System.out.println(FullName);
+
+        //task 2//
+
+        String FullName_upper_case = FullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета —  " + FullName_upper_case );
+
+        //task 3//
+
+        String fullname = "Иванов Семён Семёнович";
+        String L = "е";
+        System.out.println(fullname.replace("ё","е"));
+
     }
 }
